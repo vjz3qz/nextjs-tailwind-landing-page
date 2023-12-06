68b83b7823223712494644a107061aaacb07b797
Rahul Kumar <rakumar2003@gmail.com>
2023-12-05 23:41:26 -0800

    Update Testimonials.jsx

--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -8,7 +8,7 @@ function Testimonials() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-4 md:pb-8">
             <h2 className="h2 font-allianceMedium">
-              Trusted by companies nation-wide
+              Backed by
             </h2>
             {/* <p className="text-xl text-gray-600" data-aos="zoom-y-out">Arcu cursus vitae congue mauris rhoncus viverra nibh cras pulvinar mattis
                 blandit libero cursus mattis.</p> */}
